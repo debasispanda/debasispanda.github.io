1e414d3da6f7ac0e6e8422e2d2d1e6376310cf6b
--global <imdebasispanda@gmail.com>
2017-12-06 07:04:35 +0530

    updated pwa config

--- js/sw.js
+++ js/sw.js
@@ -1,6 +1,15 @@
+importScripts('./sw-toolbox.js');
 importScripts('https://www.gstatic.com/firebasejs/3.6.9/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.6.9/firebase-messaging.js');
+toolbox.precache([
+	'/',
+	'dist/js/vendors.bundle.min.js',
+	'dist/js/app.bundle.min.js',
+	'dist/css/vendors.bundle.min.css',
+	'css/app.css',
+]);
 
+toolbox.router.get('/(.*)', toolbox.networkFirst);
 self.addEventListener('push', (event)=> {
 	const title = 'Push Codelab';
 	const options = {
